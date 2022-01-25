757160ec17e6531a81661ac7010e36afc563e85d
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-01-25 17:09:57 +0700

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
 
   test "should get index" do
@@ -17,11 +18,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: {  } }
+      post products_url, params: {
+      product: {
+      description: @product.description,
+      image_url: @product.image_url,
+      price: @product.price,
+      title: @title,
+      }}
     end
-
     assert_redirected_to product_url(Product.last)
   end
+
 
   test "should show product" do
     get product_url(@product)
@@ -34,7 +41,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: {  } }
+    patch product_url(@product), params: {
+    product: {
+    description: @product.description,
+    image_url: @product.image_url,
+    price: @product.price,
+    title: @title,
+    }}
     assert_redirected_to product_url(@product)
   end
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  
+  def new_product(image_url)
+    Product.new(
+      title: "My Book Title",
+      description: "yyy",
+      price: 1 ,
+      image_url: image_url )
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+            http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+      "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+      "#{image_url} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+    description: "yyy",
+    price: 1,
+    image_url: "ruby.jpg")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+
 end
