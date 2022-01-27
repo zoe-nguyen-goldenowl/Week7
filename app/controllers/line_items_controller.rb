0c8f75bb012977d135409ebf698d1de907ba8fc1
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-01-27 17:29:25 +0700

    finish cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,12 +26,13 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: "Line item was successfully created." }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
+        $counter = 0
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
@@ -70,6 +71,7 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+     
+      params.require(:line_item).permit(:product_id)
     end
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,10 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
+  $counter = 0
   def index
     @products = Product.order(:title)
     @time = Time.now
+    $counter += 1
   end
 end
