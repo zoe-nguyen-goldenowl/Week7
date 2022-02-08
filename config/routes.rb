5da31557301a49886e5e3419f9f5d8eb5adf7b3c
zoe-nguyen-goldenowl <zoe.nguyen.goldenowl@gmail.com>
2022-02-08 17:22:48 +0700

    Translate

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,31 @@
 Rails.application.routes.draw do
-  resources :orders
-  resources :line_items
-  resources :carts
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
 
-  root 'store#index', as: 'store_index'
+  get 'admin/index'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+
+  resources :users
+  
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
+   
   resources :products
   post '/decrease' => 'line_items#decreasequantity'
   
   resources :products do
     get :who_bought, on: :member
   end
-  # For details on the DSL available within this file, see
-  # https://guides.rubyonrails.org/routing.html
+
+ 
 end
